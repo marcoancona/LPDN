ed6ef225375ba0002502bc0adc63c29dfeab5f58
Marco Ancona <marco.ancona@inf.ethz.ch>
2019-07-01 15:31:48 +0200

    0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="lpdn",
-    version="0.0.1",
+    version="0.0.2",
     author="Marco Ancona",
     author_email="marco.ancona@inf.ethz.ch",
     description="Implementation of Lightweight Probabilistic Deep Network (inference-only) for Keras and Tensorflow",
