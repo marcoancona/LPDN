095336227b6c56462a251e11952656c10c3f8bf3
Marco Ancona <marco.ancona@inf.ethz.ch>
2019-07-01 15:51:59 +0200

    minor

--- lpdn/__init__.py
+++ lpdn/__init__.py
@@ -1,6 +1,7 @@
 from .utils.conversion import convert_to_lpdn
+from .layers.activation import filter_activation
 from .layers.pooling import LPMaxPooling2D, LPAveragePooling1D
 from .layers.dense import LPDense
 from .layers.convolution import LPConv1D, LPConv2D
-from .layers.activation import LPActivation, filter_activation
+from .layers.activation import LPActivation
 from .layers.reshape import LPFlatten
